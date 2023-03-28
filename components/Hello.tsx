0170b5ab679d5393c331a164c563cea60995d904
m-goos <10080192+m-goos@users.noreply.github.com>
2023-03-28 12:09:10 +0200

    style: formats project with new prettier tailwind plugin

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -3,13 +3,13 @@ import profilePic from '../public/profilePic.jpeg';
 
 export default function Hello() {
   return (
-    <div className="flex flex-col justify-center items-center space-y-4">
+    <div className="flex flex-col items-center justify-center space-y-4">
       <Image
         src={profilePic}
         alt="Picture of the author"
-        className="w-20 h-20 rounded-full"
+        className="h-20 w-20 rounded-full"
       />
-      <p className="text-md md:text-lg font-medium">Hello, welcome!</p>
+      <p className="text-md font-medium md:text-lg">Hello, welcome!</p>
     </div>
   );
 }
--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -10,7 +10,7 @@ export default function NavItem({ href, text, title = '' }: NavItemProps) {
   return (
     <Link
       href={href}
-      className="py-2 px-2 rounded-md target:bg-red-100 hover:bg-slate-300 active:bg-slate-400 focus:outline-none focus:ring focus:ring-slate-300"
+      className="rounded-md py-2 px-2 target:bg-red-100 hover:bg-slate-300 focus:outline-none focus:ring focus:ring-slate-300 active:bg-slate-400"
       title={title}
     >
       {text}
