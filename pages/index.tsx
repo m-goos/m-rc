18460676a60205ceaa36934d86b91a6d1ac03a5a
m-goos <10080192+m-goos@users.noreply.github.com>
2023-03-29 10:17:47 +0200

    refactor: update eslint config and fix resulting errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
 import Layout from '@/components/Layout';
 import Hello from '@/components/Hello';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
