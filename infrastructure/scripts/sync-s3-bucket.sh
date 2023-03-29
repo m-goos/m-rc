238b8ead6f271ae06444cd8ff7218bc8b8d84beb
m-goos <10080192+m-goos@users.noreply.github.com>
2023-03-29 14:07:33 +0200

    build: add build and export command, update build directory, show link to S3 bucket

--- infrastructure/scripts/sync-s3-bucket.sh
+++ infrastructure/scripts/sync-s3-bucket.sh
@@ -4,23 +4,24 @@ export AWS_PROFILE=marc-personal
 echo "ℹ️  AWS Profile set to: ${AWS_PROFILE}"
 echo
 
-echo "ℹ️  Your account has the following S3 buckets:"
-echo
-aws s3 ls
+S3_BUCKET_NAME=marc-test-bucket-mrc
+echo "ℹ️  S3_BUCKET_NAME set to ${S3_BUCKET_NAME}"
 echo
 
-echo "❔  Please enter a name for the S3 bucket to sync to"
-read "-pS3_BUCKET_NAME: " S3_BUCKET_NAME
-echo "ℹ️  S3_BUCKET_NAME set to ${S3_BUCKET_NAME}"
+echo "⚒  Building and exporting"
+npm run build && npm run export
 echo
 
 echo "🗑  Cleaning up S3 bucket: deleting all files recursively"
 aws s3 rm s3://$S3_BUCKET_NAME --recursive
 echo
 
-BUILD_DIRECTORY=.next/static
+BUILD_DIRECTORY=out/
 echo "ℹ️  Configured build directory to sync with S3: ${BUILD_DIRECTORY}"
 echo
 
 echo "🚀  uploading to S3 "
 aws s3 sync $BUILD_DIRECTORY s3://$S3_BUCKET_NAME
+
+echo
+echo "🔗  visit at http://${S3_BUCKET_NAME}.s3-website.eu-central-1.amazonaws.com/"
