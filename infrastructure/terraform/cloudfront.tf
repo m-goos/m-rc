54be9fcb3373a66e680a7a80c9e810cb4a259cc5
m-goos <10080192+m-goos@users.noreply.github.com>
2023-04-22 20:36:11 +0200

    docs: start setting up cloudfront for S3

---
+++ infrastructure/terraform/cloudfront.tf
@@ -0,0 +1,9 @@
+# module "cloudfront" {
+#   source  = "terraform-aws-modules/cloudfront/aws"
+#   version = "3.2.1"
+# }
+
+# TODO - notes:
+# - work-in-progress, to pick up in combination `s3-private.tf`
+# - tie together cloudfront and S3
+# - useful resource: https://awstip.com/how-to-setup-static-web-hosting-using-s3-and-cloudfront-through-terraform-392a6e1dd29d
