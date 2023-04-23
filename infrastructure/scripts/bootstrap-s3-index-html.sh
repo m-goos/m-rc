9e07a6c817cf0818140f3dc1a78efb64891a2232
m-goos <10080192+m-goos@users.noreply.github.com>
2023-04-23 21:51:34 +0200

    ci: add script to bootstrap s3 and cloudfront index.html

---
+++ infrastructure/scripts/bootstrap-s3-index-html.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export AWS_PROFILE=marc-personal
+echo "ℹ️  AWS Profile set to: ${AWS_PROFILE}"
+echo
+
+export CLOUDFRONT_ID=EWXWBUO12ME1G
+echo "ℹ️  Cloudfront ID set to: ${CLOUDFRONT_ID}"
+echo
+
+# needs index.html in root
+aws s3api put-object --bucket m-rc-test-private --key index.html --body index.html --content-type text/html
+
+echo "🧨  creating cloudfront invalidation"
+aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_ID} --paths "/*"
