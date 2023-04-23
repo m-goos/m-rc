41233dcd8724676f2d6e42fa5742c36a439dbfb6
m-goos <10080192+m-goos@users.noreply.github.com>
2023-04-23 22:19:54 +0200

    feat(infra): add private S3 bucket to test with Cloudfront

--- infrastructure/terraform/s3-private.tf
+++ infrastructure/terraform/s3-private.tf
@@ -2,7 +2,7 @@ locals {
   s3_website_name_cloudfront = "m-rc-test-private"
 }
 
-module "s3_bucket" {
+module "s3_m-rc_test-private" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "3.8.2"
 
@@ -12,5 +12,29 @@ module "s3_bucket" {
   tags = {
     project = var.project_name_tag
   }
+}
 
+# S3 Policy to restrict access to Cloudfront distribution
+data "aws_iam_policy_document" "s3_policy" {
+  # Origin Access Controls
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_m-rc_test-private.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_m-rc_test-private.s3_bucket_id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
