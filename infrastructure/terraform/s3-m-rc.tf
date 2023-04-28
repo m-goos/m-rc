b11ee251dde9ed7f80c513455c9e95cf03579310
m-goos <10080192+m-goos@users.noreply.github.com>
2023-04-28 15:56:59 +0200

    chore: update aws provider and s3 module version - now blocks public access by default

--- infrastructure/terraform/s3-m-rc.tf
+++ infrastructure/terraform/s3-m-rc.tf
@@ -4,10 +4,10 @@ locals {
 
 module "s3_m-rc" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.8.2"
+  version = "3.10"
 
   bucket = local.s3_website_name_cloudfront
-  # acl    = "private" @see https://github.com/terraform-aws-modules/terraform-aws-s3-bucket/issues/223
+  # acl    = "private" @see https://github.com/terraform-aws-modules/terraform-aws-s3-bucket/issues/223 --> now private by default
 
   tags = {
     project = var.project_name_tag
