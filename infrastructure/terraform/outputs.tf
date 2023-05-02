18975bbf2a1e28ea1c44542fe86c13e5393a8468
m-goos <10080192+m-goos@users.noreply.github.com>
2023-05-02 17:44:31 +0200

    refactor: move all outputs to one file

---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,27 @@
+# Cloudfront
+output "cloudfront_distribution_id" {
+  description = "The identifier for the distribution."
+  value       = module.cloudfront.cloudfront_distribution_id
+}
+
+output "cloudfront_distribution_domain_name" {
+  description = "The domain name corresponding to the distribution."
+  value       = module.cloudfront.cloudfront_distribution_domain_name
+}
+
+# S3
+output "s3_bucket_bucket_regional_domain_name" {
+  description = "The bucket region-specific domain name. The bucket domain name including the region name, please refer here for format. Note: The AWS CloudFront allows specifying S3 region-specific endpoint when creating S3 origin, it will prevent redirect issues from CloudFront to S3 Origin URL."
+  value       = module.s3_m-rc.s3_bucket_bucket_regional_domain_name
+}
+
+# ACM
+output "distinct_domain_names" {
+  description = "List of distinct domains names used for the validation."
+  value       = module.acm.distinct_domain_names
+}
+
+output "validation_domains" {
+  description = "List of distinct domain validation options. This is useful if subject alternative names contain wildcards."
+  value       = module.acm.validation_domains
+}
