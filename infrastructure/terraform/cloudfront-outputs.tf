ffd4863aefd257960e5b3588de10cdc5cb435139
m-goos <10080192+m-goos@users.noreply.github.com>
2023-05-02 17:44:31 +0200

    refactor: move all outputs to one file

--- infrastructure/terraform/cloudfront-outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_distribution_id" {
-  description = "The identifier for the distribution."
-  value       = module.cloudfront.cloudfront_distribution_id
-}
-
-output "cloudfront_distribution_domain_name" {
-  description = "The domain name corresponding to the distribution."
-  value       = module.cloudfront.cloudfront_distribution_domain_name
-}
