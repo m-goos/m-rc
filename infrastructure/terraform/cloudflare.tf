a29092caa2b3f3311d9b0839c872d7fa29d332ce
m-goos <10080192+m-goos@users.noreply.github.com>
2023-05-05 16:35:50 +0200

    docs: add some notes for next steps and troubleshooting

--- infrastructure/terraform/cloudflare.tf
+++ infrastructure/terraform/cloudflare.tf
@@ -1,14 +1,17 @@
-
-# TODO next:
+# FOR REFERENCE:
 # https://github.com/terraform-aws-modules/terraform-aws-acm/tree/v4.3.2/examples/complete-dns-validation-with-cloudflare
-
-
-# next steps, follow this:
 # Providing simple steps in UI: https://advancedweb.hu/how-to-use-a-custom-domain-on-cloudfront-with-cloudflare-managed-dns/
 
 # Stuck on the following error:
 # https://developers.cloudflare.com/dns/manage-dns-records/troubleshooting/records-with-same-name/
 
+# Removed an AAAA record with the following content:
+# (see google keep)
+# NEXT STEP: just do an apply again, I manually removed the AAAA record
+# an apply did not work last time - the record might need a minute to disappear from DNS
+
+# TODO: Fix this error from terraform fmt in the githook for commits:
+# Error: registry.terraform.io/cloudflare/cloudflare: the cached package for registry.terraform.io/cloudflare/cloudflare 4.4.0 (in .terraform/providers) does not match any of the checksums recorded in the dependency lock file
 resource "cloudflare_record" "validation" {
   count = length(module.acm.distinct_domain_names)
 
@@ -28,6 +31,7 @@ resource "cloudflare_record" "m-rc" {
   value   = module.cloudfront.cloudfront_distribution_domain_name
   type    = "CNAME"
   ttl     = 60
+  proxied = false
 }
 
 data "cloudflare_zone" "m-rc" {
