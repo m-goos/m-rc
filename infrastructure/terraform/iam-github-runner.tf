4faab33ff903d91ca1dab1ccbdfdce0c8ccac86f
m-goos <10080192+m-goos@users.noreply.github.com>
2023-05-11 20:36:16 +0200

    feat: create IAM role for github runner

---
+++ infrastructure/terraform/iam-github-runner.tf
@@ -0,0 +1,20 @@
+# TODO eventually replace with https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest ?
+
+module "s3_user" {
+  source  = "cloudposse/iam-s3-user/aws"
+  version = "1.1.0"
+  tags = {
+    project = var.project_name_tag
+  }
+
+  # IAM role information
+  namespace = "m-rc"
+  stage     = "prod"
+  name      = "github-runner-s3-deploy"
+
+  s3_actions   = ["s3:PutObject", "s3:GetObject", "s3:ListBucket", "s3:DeleteObject"]
+  s3_resources = ["${module.s3_m-rc.s3_bucket_arn}/*"]
+
+  ssm_base_path = var.ssm_base_path
+  ssm_enabled   = var.ssm_enabled
+}
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -21,3 +21,20 @@ variable "cloudflare_api_token" {
   type      = string
   sensitive = true
 }
+
+# configure iam role for github runner
+variable "ssm_enabled" {
+  type        = bool
+  description = <<-EOT
+    Set `true` to store secrets in SSM Parameter Store,
+    `false` to store secrets in Terraform state as outputs.
+    Since Terraform state would contain the secrets in plaintext,
+    use of SSM Parameter Store is recommended.
+    EOT
+  default     = true
+}
+
+variable "ssm_base_path" {
+  type        = string
+  description = "Prefix for ssm parameters"
+}
