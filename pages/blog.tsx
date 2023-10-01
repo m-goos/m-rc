bc13622a13b11d56226167021bf2e6855e219802
m-goos <10080192+m-goos@users.noreply.github.com>
2023-10-01 12:26:30 +0200

    feat: very basic MDX configuration

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,5 +1,11 @@
 import Layout from '@/components/Layout';
+import HelloWorld from '@/posts/hello-world/hello-world.mdx';
 
 export default function Blog() {
-  return <Layout>Welcome to my blog.</Layout>;
+  return (
+    <Layout>
+      Welcome to my blog.
+      <HelloWorld />
+    </Layout>
+  );
 }
