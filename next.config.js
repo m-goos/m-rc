2a169fef0bf545fcfbac8243eee555543fd4f902
m-goos <10080192+m-goos@users.noreply.github.com>
2023-10-01 12:26:30 +0200

    feat: very basic MDX configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 const withExportImages = require('next-export-optimize-images');
+const withMDX = require('@next/mdx')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    // required to enable MDX
+    mdxRs: true,
+  },
 };
 
 /**
  * `withExportImages` optimizes images for `next export` and solves:
  * @see https://github.com/vercel/next.js/discussions/19065
  */
-module.exports = withExportImages(nextConfig);
+withExportImages(nextConfig);
+
+module.exports = withMDX(withExportImages);
