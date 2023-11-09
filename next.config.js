3be9c70d495622aea364ae04e9e80da5fe8c5b76
marc-goossens <m.goossens@elsevier.com>
2023-11-09 21:45:08 +0100

    chore: update deprecated export command, use export in nextConfig instead

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withExportImages = require('next-export-optimize-images');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
 };
 
 /**
