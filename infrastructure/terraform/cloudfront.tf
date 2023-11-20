32b301225dcf61edb665fc3b64aa2b4ce11947c8
m-goos <10080192+m-goos@users.noreply.github.com>
2023-11-20 15:32:21 +0100

    feat(infra): update cloudfront 404 page config to actually serve 404

--- infrastructure/terraform/cloudfront.tf
+++ infrastructure/terraform/cloudfront.tf
@@ -45,10 +45,10 @@ module "cloudfront" {
   custom_error_response = [{
     error_code         = 404
     response_code      = 200
-    response_page_path = "/index.html"
+    response_page_path = "/404.html"
     }, {
     error_code         = 403
     response_code      = 200
-    response_page_path = "/index.html"
+    response_page_path = "/404.html"
   }]
 }
