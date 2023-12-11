bcd38e7944b35456bc22135443996b8e1946e33d
m-goos <10080192+m-goos@users.noreply.github.com>
2023-12-11 16:58:03 +0800

    chore: migrate to new Head package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,17 @@
-import Head from 'next/head';
 import Layout from '@/components/Layout';
 import Hello from '@/components/Hello';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Marc Goossens',
+  description: 'Personal blog on software development by Marc Goossens',
+};
+
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Marc Goossens</title>
-        <meta
-          name="description"
-          content="Personal blog on software development by Marc Goossens"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Hello />
-      </Layout>
-    </>
+    <Layout>
+      <Hello />
+    </Layout>
   );
 }
