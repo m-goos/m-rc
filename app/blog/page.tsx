c9eada743b73336f7ce02385a65fb2ca0bf26e65
m-goos <10080192+m-goos@users.noreply.github.com>
2024-02-09 16:28:46 +0100

    refactor: move mdx file and update blog for mdx nextjs 14

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,3 @@
-import HelloWorld from '@/posts/hello-world/hello-world.mdx';
-
 export default function Page() {
-  return (
-    <>
-      Welcome to my blog.
-      <HelloWorld />
-    </>
-  );
+  return <>Welcome to my blog.</>;
 }
