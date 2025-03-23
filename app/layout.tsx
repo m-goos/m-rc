cfbcb72e1964c761371741e2ad3638031d57dab8
m-goos <10080192+m-goos@users.noreply.github.com>
2025-03-23 23:58:32 +0100

    feat(layout): make top-level layout slightly wider, from xl to 2xl
    The layout was a bit squished, as in, not wide enough; already on laptop screens. This small tweak
    allows the website to be a little wider, which is better when reading blog posts on laptop screens.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <body>
         <div className="flex flex-col items-center min-h-screen bg-slate-50">
           {/* this container sets a flexible max width that's always maxed out */}
-          <div className="flex grow max-w-xl w-full">
+          <div className="flex grow max-w-2xl w-full">
             {/* provide padding and keep blogs constrained to 100% width */}
             <div className="flex flex-col w-full px-4">
               <Nav />
