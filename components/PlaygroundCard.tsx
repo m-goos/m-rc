325debb5ed67b9dc9c73ed9c8b8b64e862744e3c
m-goos <10080192+m-goos@users.noreply.github.com>
2025-10-30 00:10:48 +0100

    feat(playground-card): add component, introduce some colour to the blog; quick-n-dirty w tailwind

--- components/PlaygroundCard.tsx
+++ components/PlaygroundCard.tsx
@@ -1,6 +1,50 @@
+import Image from 'next/image';
 import { Project } from 'app/playground/page';
 
 export default function PlaygroundCard({ project }: { project: Project }) {
-  // TODO - cards
-  return `Hi from ${project}`;
+  return (
+    <article className="flex flex-col flex-1 min-w-[300px] bg-slate-100 rounded-xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 border border-gray-200">
+      <div className="relative bg-gray-100 aspect-[16/10]">
+        <Image
+          src={project.screenshot}
+          alt={`Screenshot of ${project.title}`}
+          width={960}
+          height={667}
+          className="object-cover"
+        />
+      </div>
+
+      <div className="flex flex-col  flex-1 justify-between p-5">
+        {/* description */}
+        <div>
+          <h2 className="text-lg font-semibold text-gray-900 mb-2">
+            {project.title}
+          </h2>
+          <p className="text-sm text-gray-600 mb-4">{project.description}</p>
+        </div>
+
+        {/* links */}
+        <div className="flex flex-wrap gap-3">
+          <a
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-sm font-medium text-blue-600 hover:text-blue-800 transition-colors"
+          >
+            Live Demo →
+          </a>
+          {project.repository && (
+            <a
+              href={project.repository}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm font-medium text-cyan-600 hover:text-cyan-900 transition-colors"
+            >
+              Repository →
+            </a>
+          )}
+        </div>
+      </div>
+    </article>
+  );
 }
